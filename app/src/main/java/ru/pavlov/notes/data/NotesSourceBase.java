11f91010756f16beb10076dacac4c22de856ad1d
pavlov <pavlov@studiofact.ru>
2021-08-22 20:57:56 +0500

    Урок 11

--- app/src/main/java/ru/pavlov/notes/data/NotesSourceBase.java
+++ app/src/main/java/ru/pavlov/notes/data/NotesSourceBase.java
@@ -3,6 +3,7 @@ package ru.pavlov.notes.data;
 import java.util.Calendar;
 
 public abstract class NotesSourceBase implements NotesSource {
+
     public NoteData getNewNoteData() {
         Calendar calendar = Calendar.getInstance();
         return new NoteData("", "", calendar);
